9954d92d6ad1df55120361298988653826ac31f1
Taaylored <tprince09@eagles.ewu.edu>
2016-07-22 17:36:59 -0700

    major updates across the site

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//$('#pickandchoose').hide();
+//
+//// $('button').click( function(){
+//  
+////   // console.log('ok')
+////   $('p').slideToggle();
+//  
+//  
+//// });
+//
+//$('.pickandchoose').click( function(){
+//  
+//  // console.log('ok')
+//  $('#pickandchoose').slideToggle();
+//  
+//  
+//});
